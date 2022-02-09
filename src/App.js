745ae4ef89e79cb49e3415ae78cf0d177041625d
Shamizen <dev.shamizen@gmail.com>
2022-02-09 20:24:39 +0700

    remove unused  useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import data from './data';
-import { useState } from 'react';
 import Content from './components/Content';
 
 function App() {
-  const [content, setContent] = useState(data);
   return (
     <main>
       <div className='container'>
         <h3>เครื่องมือพัฒนาเว็บไซต์ปี 2021</h3>
         <section>
-          {content.map((data) => {
-            return <Content key={data.id} {...data} />;
+          {data.map((element) => {
+            return <Content key={element.id} {...element} />;
           })}
         </section>
       </div>
